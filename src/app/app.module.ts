f37e47e4035abb4961af6e2143fe0c2a196af503
mosaririri <dos.quoi813@gmail.com>
2019-12-28 12:01:07 +0900

    @angular/animationsをimport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -27,6 +28,7 @@ import { LoginComponent } from './components/login/login.component';
     MatFormFieldModule,
     MatIconModule,
     FormsModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
